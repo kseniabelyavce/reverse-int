be43c569765d280304a606940a95e4965e1ee943
Kseniya Belyavtseva <kseniyabelyavtseva@protonmail.com>
2020-09-17 22:10:32 +0300

    feat: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  if (n < 10 && n >= 0) {
+    return n;
+  }
+  else {
+    let chars = Math.abs(n).toString().split('')
+
+    for (let l = 0, r = chars.length - 1; l < r; l++, r--) {
+      [chars[l], chars[r]] = [chars[r], chars[l]]
+    }
+
+    return parseInt(chars.join(''), 10)
+  }
 }
+
+
